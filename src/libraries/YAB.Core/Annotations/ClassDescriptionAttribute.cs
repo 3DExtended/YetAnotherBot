65933a03e3282bafa3e9e9dcc896d05a5baa2eb6
Peter Esser <me@peter-esser.de>
2021-11-27 22:01:26 +0100

    Add annotations to events and base classes

---
+++ src/libraries/YAB.Core/Annotations/ClassDescriptionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace YAB.Core.Annotations
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class ClassDescriptionAttribute : Attribute
+    {
+        public ClassDescriptionAttribute(string description)
+        {
+            Description = description;
+        }
+
+        public string Description { get; set; }
+    }
+}
---
+++ src/libraries/YAB.Core/Annotations/PropertyDescriptionAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace YAB.Core.Annotations
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class PropertyDescriptionAttribute : Attribute
+    {
+        public PropertyDescriptionAttribute(bool isSecret, string description)
+        {
+            IsSecret = isSecret;
+            Description = description;
+        }
+
+        public string Description { get; set; }
+
+        public bool IsSecret { get; set; }
+    }
+}
